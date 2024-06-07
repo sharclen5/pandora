d827b9afb19ab7f21072ba8a645ae19bc8251ea7
sharclen5 <nicholashasian@gmail.com>
2024-06-07 13:43:06 +0700

    Nambah layout navbar baru, buat kalo belum login

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,7 +2,24 @@
 <div class="fixed top-0 z-50 flex flex-wrap place-items-center overflow-visible">
     <section class="relative mx-auto">
 
-        <!-- navbar -->
+        <!-- navbar_belum-log-in -->
+        <nav class="flex justify-between bg-gray-900 text-white w-screen">
+            <div class="px-4 xl:px-12 py-2 flex w-full items-center">
+                <a class="text-2xl font-bold font-heading flex items-center" href="/">
+                    <img class="h-10" src="logo.png" alt="logo"> Pandora
+                </a>
+            </div>
+
+            <!-- Login/Register button -->
+            <div class="ml-2">
+                <button type="button"
+                    class="mt-2 mr-8 text-white hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+                    onclick="window.location='{{ route('login') }}'">
+                    Login/Register</button>
+            </div>
+        </nav>
+
+        {{-- <!-- navbar_logged-in -->
         <nav class="flex justify-between bg-gray-900 text-white w-screen">
             <div class="px-4 xl:px-12 py-2 flex w-full items-center">
                 <a class="text-2xl font-bold font-heading flex items-center" href="/">
@@ -32,8 +49,8 @@
                     <button id="dropdownNotificationButton" data-dropdown-toggle="dropdownNotification"
                         class="relative inline-flex items-center text-sm font-medium text-center text-gray-500 hover:text-gray-900 focus:outline-none dark:hover:text-white dark:text-gray-400"
                         type="button">
-                        <svg class="text-white w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                            viewBox="0 0 14 20">
+                        <svg class="text-white w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 14 20">
                             <path
                                 d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
                         </svg>
@@ -54,12 +71,13 @@
                         <div class="divide-y divide-gray-100 dark:divide-gray-700">
                             <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
                                 <div class="flex-shrink-0">
-                                    <img class="rounded-full w-11 h-11" src="/docs/images/people/profile-picture-1.jpg"
-                                        alt="Jese image">
+                                    <img class="rounded-full w-11 h-11"
+                                        src="/docs/images/people/profile-picture-1.jpg" alt="Jese image">
                                     <div
                                         class="absolute flex items-center justify-center w-5 h-5 ms-6 -mt-5 bg-blue-600 border border-white rounded-full dark:border-gray-800">
                                         <svg class="w-2 h-2 text-white" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                            viewBox="0 0 18 18">
                                             <path
                                                 d="M1 18h16a1 1 0 0 0 1-1v-6h-4.439a.99.99 0 0 0-.908.6 3.978 3.978 0 0 1-7.306 0 .99.99 0 0 0-.908-.6H0v6a1 1 0 0 0 1 1Z" />
                                             <path
@@ -76,12 +94,13 @@
                             </a>
                             <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
                                 <div class="flex-shrink-0">
-                                    <img class="rounded-full w-11 h-11" src="/docs/images/people/profile-picture-2.jpg"
-                                        alt="Joseph image">
+                                    <img class="rounded-full w-11 h-11"
+                                        src="/docs/images/people/profile-picture-2.jpg" alt="Joseph image">
                                     <div
                                         class="absolute flex items-center justify-center w-5 h-5 ms-6 -mt-5 bg-gray-900 border border-white rounded-full dark:border-gray-800">
                                         <svg class="w-2 h-2 text-white" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                            viewBox="0 0 20 18">
                                             <path
                                                 d="M6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Zm11-3h-2V5a1 1 0 0 0-2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 0 0 2 0V9h2a1 1 0 1 0 0-2Z" />
                                         </svg>
@@ -89,20 +108,23 @@
                                 </div>
                                 <div class="w-full ps-3">
                                     <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400"><span
-                                            class="font-semibold text-gray-900 dark:text-white">Joseph Mcfall</span> and
+                                            class="font-semibold text-gray-900 dark:text-white">Joseph Mcfall</span>
+                                        and
                                         <span class="font-medium text-gray-900 dark:text-white">5 others</span> started
-                                        following you.</div>
+                                        following you.
+                                    </div>
                                     <div class="text-xs text-blue-600 dark:text-blue-500">10 minutes ago</div>
                                 </div>
                             </a>
                             <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
                                 <div class="flex-shrink-0">
-                                    <img class="rounded-full w-11 h-11" src="/docs/images/people/profile-picture-3.jpg"
-                                        alt="Bonnie image">
+                                    <img class="rounded-full w-11 h-11"
+                                        src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image">
                                     <div
                                         class="absolute flex items-center justify-center w-5 h-5 ms-6 -mt-5 bg-red-600 border border-white rounded-full dark:border-gray-800">
                                         <svg class="w-2 h-2 text-white" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                            viewBox="0 0 20 18">
                                             <path
                                                 d="M17.947 2.053a5.209 5.209 0 0 0-3.793-1.53A6.414 6.414 0 0 0 10 2.311 6.482 6.482 0 0 0 5.824.5a5.2 5.2 0 0 0-3.8 1.521c-1.915 1.916-2.315 5.392.625 8.333l7 7a.5.5 0 0 0 .708 0l7-7a6.6 6.6 0 0 0 2.123-4.508 5.179 5.179 0 0 0-1.533-3.793Z" />
                                         </svg>
@@ -112,18 +134,20 @@
                                     <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400"><span
                                             class="font-semibold text-gray-900 dark:text-white">Bonnie Green</span> and
                                         <span class="font-medium text-gray-900 dark:text-white">141 others</span> love
-                                        your story. See it and view more stories.</div>
+                                        your story. See it and view more stories.
+                                    </div>
                                     <div class="text-xs text-blue-600 dark:text-blue-500">44 minutes ago</div>
                                 </div>
                             </a>
                             <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
                                 <div class="flex-shrink-0">
-                                    <img class="rounded-full w-11 h-11" src="/docs/images/people/profile-picture-4.jpg"
-                                        alt="Leslie image">
+                                    <img class="rounded-full w-11 h-11"
+                                        src="/docs/images/people/profile-picture-4.jpg" alt="Leslie image">
                                     <div
                                         class="absolute flex items-center justify-center w-5 h-5 ms-6 -mt-5 bg-green-400 border border-white rounded-full dark:border-gray-800">
                                         <svg class="w-2 h-2 text-white" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                            viewBox="0 0 20 18">
                                             <path
                                                 d="M18 0H2a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h2v4a1 1 0 0 0 1.707.707L10.414 13H18a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5 4h2a1 1 0 1 1 0 2h-2a1 1 0 1 1 0-2ZM5 4h5a1 1 0 1 1 0 2H5a1 1 0 0 1 0-2Zm2 5H5a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Zm9 0h-6a1 1 0 0 1 0-2h6a1 1 0 1 1 0 2Z" />
                                         </svg>
@@ -131,7 +155,8 @@
                                 </div>
                                 <div class="w-full ps-3">
                                     <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400"><span
-                                            class="font-semibold text-gray-900 dark:text-white">Leslie Livingston</span>
+                                            class="font-semibold text-gray-900 dark:text-white">Leslie
+                                            Livingston</span>
                                         mentioned you in a comment: <span class="font-medium text-blue-500"
                                             href="#">@bonnie.green</span> what do you say?</div>
                                     <div class="text-xs text-blue-600 dark:text-blue-500">1 hour ago</div>
@@ -211,7 +236,7 @@
             <div class="ml-2">
                 Name Here
             </div>
-        </nav>
+        </nav> --}}
 
     </section>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,9 @@
             seamless <span class="text-blue-400">connections</span> – all in one place.</p>
 
         <button type="button"
-            class="mt-20 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm sm:text-base md:text-xl px-4 sm:px-6 md:px-10 py-2 sm:py-2.5 md:py-3.5 text-center me-2 mb-2">Get
-            Started</button>
+            class="mt-20 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm sm:text-base md:text-xl px-4 sm:px-6 md:px-10 py-2 sm:py-2.5 md:py-3.5 text-center me-2 mb-2"
+            onclick="window.location='{{ route('login') }}'">
+            Get Started</button>
         <p class="mb-48"></p>
 
     </div>
