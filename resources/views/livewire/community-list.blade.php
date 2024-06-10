72f73d59e9321bfea1b29aff6567502cf2af54d1
sharclen5 <nicholashasian@gmail.com>
2024-06-10 11:38:54 +0700

    Benerin modal view community

--- resources/views/livewire/community-list.blade.php
+++ resources/views/livewire/community-list.blade.php
@@ -1,11 +1,11 @@
 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mb-8 mt-8">
     @foreach($community as $commun)
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <a wire:navigate href="community/{{ $commun->id }}" data-modal-target="static-modal" data-modal-toggle="static-modal">
+        <a href="#" data-modal-target="detail-modal-{{ $commun->id }}" data-modal-toggle="detail-modal-{{ $commun->id }}">
             <img class="rounded-t-lg" src="{{ asset('storage/' .$commun->img) }}" alt="" style="width: 100%; height: 165px;" />
         </a>
         <div class="p-5">
-            <a href="#" data-modal-target="static-modal" data-modal-toggle="static-modal">
+            <a href="#" data-modal-target="detail-modal-{{ $commun->id }}" data-modal-toggle="detail-modal-{{ $commun->id }}">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                     {{ $commun->name }}
                 </h5>
@@ -16,4 +16,6 @@
         </div>
     </div>
     @endforeach
+
+    @include('livewire.community-profile', ['id' => $commun->id])
 </div>
