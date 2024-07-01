4a347d5cd733013ac6e9e656d341193dcf06ce33
katsuku27 <148852467+katsuku27@users.noreply.github.com>
2024-07-01 18:02:47 +0700

    fix conflict

--- resources/views/livewire/recomendation.blade.php
+++ resources/views/livewire/recomendation.blade.php
@@ -5,7 +5,7 @@
             <!-- Item 1 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="{{ asset('storage/' . $recomend->img) }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    class="absolute block w-full h-full  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                     alt="...">
             </div>    
         @endforeach
